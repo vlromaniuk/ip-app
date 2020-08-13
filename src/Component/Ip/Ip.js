435b2dbff7a161214eba3fbb9e214c3edac2e53d
vlromaniuk <vlad96r@protonmail.com>
2020-08-13 12:17:36 +0300

    finish app

---
+++ src/Component/Ip/Ip.js
@@ -0,0 +1,55 @@
+import React, {Component} from "react";
+import 'materialize-css/dist/css/materialize.css';
+import './Ip.css'
+
+export default class Ip extends Component {
+    state= {
+        item: []
+    }
+
+    componentDidMount() {
+        fetch( 'https://freegeoip.app/json/')
+            .then(res => res.json())
+            .then(json => {
+                console.log(json)
+                this.setState({
+                    item: json
+                })
+            })
+    }
+
+
+    render() {
+        // const ip = this.state.item.map(((value, key) =>
+        //     <span>{value}</span>
+        // ))
+        return (
+            <div className="Ip">
+                <table className="bordered centered">
+                    <tbody>
+                        <tr>
+                            <td>Ip адресс:</td>
+                            <td>{this.state.item.ip}</td>
+                        </tr>
+                        <tr>
+                            <td>Страна:</td>
+                            <td>{this.state.item.country_name}</td>
+                        </tr>
+                        <tr>
+                            <td>Область:</td>
+                            <td>{this.state.item.region_name}</td>
+                        </tr>
+                        <tr>
+                            <td>Город:</td>
+                            <td>{this.state.item.city}</td>
+                        </tr>
+                        <tr>
+                            <td>Индекс:</td>
+                            <td>{this.state.item.zip_code}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
